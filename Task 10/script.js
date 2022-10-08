9301ec83ac06d9b03a6df1f917329ccce77d87e7
inga nenortaite <inga.nenortaite@gmail.com>
2022-10-09 01:05:37 +0300

    Task 10 FINISHED

--- Task 10/script.js
+++ Task 10/script.js
@@ -11,3 +11,9 @@ console.log(a);
 console.log(b);
 console.log(c);
 console.log(c);
+
+import { composition } from './modules/math/composition.js';
+import { division } from './modules/math/division.js';
+import { multiplication } from './modules/math/multiplication.js';
+import { substraction } from './modules/math/subtraction.js';
+import { one, two, three, four, five } from './modules/numbers/numbers.js';
