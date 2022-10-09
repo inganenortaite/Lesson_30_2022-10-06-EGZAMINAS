bf2e537b85f1ab6ee2b343f9e7030f5e7eb9d85d
inga nenortaite <inga.nenortaite@gmail.com>
2022-10-09 20:55:31 +0300

    Task 8 FINISHED

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+function Calculator() {
+    this.sum = function(num1, num2) {
+        return num1 + num2;
+    }
+
+    this.substraction = function(num1, num2){
+        return num1 - num2;
+    }
+
+    this.multiplication = function(num1, num2){
+        return num1 * num2;
+    }
+
+    this.division = function(num1, num2){
+        return num1 / num2;
+    }
+}
+
+let calc = new Calculator();
+console.log(calc.sum(2, 5));
+console.log(calc.substraction(15, 5));
+console.log(calc.multiplication(162, 5));
+console.log(calc.division(800, 10));
